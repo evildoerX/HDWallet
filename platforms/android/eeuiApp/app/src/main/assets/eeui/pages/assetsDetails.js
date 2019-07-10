a16df0dba7f27dcc8ebf750a82668e0613896391
CH <CH@corp-ci.com>
2019-07-10 15:06:22 +0800

    update

--- platforms/android/eeuiApp/app/src/main/assets/eeui/pages/assetsDetails.js
+++ platforms/android/eeuiApp/app/src/main/assets/eeui/pages/assetsDetails.js
@@ -73,6 +73,13 @@ if(typeof app=="undefined"){app=weex}
 var __vue_exports__, __vue_options__
 var __vue_styles__ = []
 
+/* styles */
+__vue_styles__.push(__webpack_require__(50)
+)
+
+/* script */
+__vue_exports__ = __webpack_require__(51)
+
 /* template */
 var __vue_template__ = __webpack_require__(20)
 __vue_options__ = __vue_exports__ = __vue_exports__ || {}
@@ -89,6 +96,7 @@ if (typeof __vue_options__ === "function") {
 __vue_options__.__file = "/Users/ch/BlockChain/HDWallet/src/pages/assetsDetails.vue"
 __vue_options__.render = __vue_template__.render
 __vue_options__.staticRenderFns = __vue_template__.staticRenderFns
+__vue_options__._scopeId = "data-v-380969eb"
 __vue_options__.style = __vue_options__.style || {}
 __vue_styles__.forEach(function (module) {
   for (var name in module) {
@@ -110,9 +118,257 @@ new Vue(module.exports)
 /***/ (function(module, exports) {
 
 module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
-  return _c('div', [_vm._v("assetsde")])
-},staticRenderFns: []}
+  return _c('div', {
+    staticClass: ["assetsdetails"]
+  }, [_c('navbar', {
+    staticClass: ["assetsdetails-navbar"]
+  }, [_c('navbar-item', {
+    attrs: {
+      "type": "back"
+    }
+  }), _c('navbar-item', {
+    attrs: {
+      "type": "title"
+    }
+  }, [_c('div', {
+    staticClass: ["assetsdetails-navbar-main"]
+  }, [_c('text', {
+    staticClass: ["assetsdetails-navbar-title"]
+  }, [_vm._v(_vm._s(_vm.asset.name))])])])], 1), _c('div', {
+    staticClass: ["assetsdetails-content"]
+  }, [_c('image', {
+    staticClass: ["assetsdetails-content-logo"],
+    attrs: {
+      "src": ("../assets/AssetLogo/" + (_vm.asset.name) + ".png")
+    }
+  }), _vm._m(0), _c('div', {
+    staticClass: ["assetsdetails-content_listitem"]
+  }, [_c('button', {
+    staticClass: ["assetsdetails-content_button"],
+    attrs: {
+      "eeui": {
+        text: '收款',
+        model: 'green'
+      }
+    },
+    on: {
+      "click": _vm.receiptClick
+    }
+  }), _c('button', {
+    staticClass: ["assetsdetails-content_button"],
+    attrs: {
+      "eeui": {
+        text: '转账',
+        model: 'blue'
+      }
+    },
+    on: {
+      "click": _vm.transferClick
+    }
+  })], 1), _c('div', {
+    staticClass: ["assetsdetails-content_line"]
+  }), _c('tabbar', {
+    ref: "reflectName",
+    staticClass: ["assetsdetails-content_tabs"],
+    attrs: {
+      "eeui": _vm.tabsEeui,
+      "tabPages": _vm.tabPages
+    }
+  })])], 1)
+},staticRenderFns: [function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _c('div', {
+    staticClass: ["assetsdetails-content_asset"]
+  }, [_c('text', {
+    staticClass: ["assetsdetails-content_assetvalue"]
+  }, [_vm._v("0")]), _c('text', {
+    staticClass: ["assetsdetails-content_assetusd"]
+  }, [_vm._v("≈ 0.00 USD")])])
+}]}
 module.exports.render._withStripped = true
+
+/***/ }),
+
+/***/ 50:
+/***/ (function(module, exports) {
+
+module.exports = {
+  "assetsdetails": {
+    "width": "750",
+    "flex": 1
+  },
+  "assetsdetails-navbar": {
+    "width": "750",
+    "height": "100"
+  },
+  "assetsdetails-navbar-main": {
+    "flexDirection": "row",
+    "alignItems": "center"
+  },
+  "assetsdetails-navbar-title": {
+    "fontSize": "32",
+    "color": "#ffffff"
+  },
+  "assetsdetails-navbar-right": {
+    "width": "100",
+    "height": "100",
+    "color": "#ffffff"
+  },
+  "assetsdetails-content": {
+    "marginTop": "64",
+    "width": "750",
+    "flex": 1,
+    "alignItems": "center"
+  },
+  "assetsdetails-content-logo": {
+    "width": "128",
+    "height": "128"
+  },
+  "assetsdetails-content_asset": {
+    "alignItems": "center",
+    "marginTop": "32"
+  },
+  "assetsdetails-content_assetvalue": {
+    "fontSize": "80",
+    "fontWeight": "500",
+    "color": "#333333"
+  },
+  "assetsdetails-content_assetusd": {
+    "fontSize": "40",
+    "fontWeight": "500",
+    "color": "#666666"
+  },
+  "assetsdetails-content_line": {
+    "backgroundColor": "#f8f9fa",
+    "height": "32",
+    "width": "750"
+  },
+  "assetsdetails-content_button": {
+    "fontSize": "24",
+    "marginLeft": "38",
+    "marginRight": "38",
+    "marginTop": "64",
+    "marginBottom": "20",
+    "width": "300",
+    "height": "80"
+  },
+  "assetsdetails-content_listitem": {
+    "width": "750",
+    "flexDirection": "row",
+    "marginBottom": "32"
+  },
+  "assetsdetails-content_tabs": {
+    "flex": 1,
+    "width": "750"
+  }
+}
+
+/***/ }),
+
+/***/ 51:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+var eeui = app.requireModule("eeui");
+
+exports.default = {
+  data: function data() {
+    return {
+      tabsEeui: {
+        tabType: 'top',
+        tabBackgroundColor: '#ffffff',
+        indicatorColor: '#3075ee',
+        indicatorWidth: 160,
+        underlineColor: '#e9e9e9',
+        underlineHeight: 2,
+        textSelectColor: '#3075ee',
+        textUnselectColor: '#999999',
+        textBold: 2
+      },
+      asset: {},
+      assetAddress: '0x58Df7EDadC0795519fdC7284A67547670F08C5E0',
+      tabPages: [{
+        title: '全部',
+        url: 'http://dotwe.org/raw/dist/b5fd96d8d790f0100bdfc20b93eedf09.bundle.wx'
+      }, {
+        title: '收款',
+        url: 'http://dotwe.org/raw/dist/ba938c9aaebe41e5f60b98f90bd0bf61.bundle.wx',
+        message: 9
+      }, {
+        title: '转账',
+        url: 'http://dotwe.org/raw/dist/fb6f016b0116969b6b503e1d3a35285f.bundle.wx',
+        message: 18
+      }]
+    };
+  },
+  mounted: function mounted() {
+    this.asset = app.config.params ? app.config.params : "error";
+  },
+
+  methods: {
+    receiptClick: function receiptClick() {
+      eeui.openPage({
+        url: 'receiptDetails',
+        pageType: "app",
+        params: this.asset
+      });
+    },
+    transferClick: function transferClick() {
+      eeui.openPage({
+        url: 'transferDetails',
+        pageType: "app",
+        params: this.asset
+      });
+    },
+    copyText: function copyText() {
+      eeui.copyText(this.assetAddress);
+      eeui.toast({
+        message: "复制成功",
+        gravity: "middle"
+      });
+    },
+    cropyText: function cropyText(str) {
+      var t = "";
+      t = str.slice(0, 10) + "******" + str.substring(32);
+      return t;
+    }
+  }
+};
 
 /***/ })
 
--- src/pages/assetsDetails.vue
+++ src/pages/assetsDetails.vue
@@ -1,3 +1,176 @@
 <template>
-  <div>assetsde</div>
+  <div class="assetsdetails">
+    <navbar class="assetsdetails-navbar" >
+      <navbar-item type="back"></navbar-item>
+      <navbar-item type="title">
+        <div class="assetsdetails-navbar-main">
+          <text class="assetsdetails-navbar-title">{{asset.name}}</text>
+        </div>
+      </navbar-item>
+    </navbar>
+    <div class="assetsdetails-content">
+      <image class="assetsdetails-content-logo" :src="`../assets/AssetLogo/${asset.name}.png`" ></image>
+      <div class="assetsdetails-content_asset">
+        <text class="assetsdetails-content_assetvalue">0</text>
+        <text class="assetsdetails-content_assetusd">≈ 0.00 USD</text>
+      </div>
+      <div class="assetsdetails-content_listitem">
+        <button class="assetsdetails-content_button" :eeui="{text:'收款',  model:'green'}" @click="receiptClick"></button>
+        <button class="assetsdetails-content_button" :eeui="{text:'转账',  model:'blue'}" @click="transferClick"></button>
+      </div>
+      <div class="assetsdetails-content_line"></div>
+      <tabbar
+        ref="reflectName"
+        class="assetsdetails-content_tabs"
+        :eeui="tabsEeui"
+        :tabPages="tabPages"></tabbar>
+    </div>
+  </div>
 </template>
+
+<script>
+var eeui = app.requireModule("eeui");
+
+export default {
+  data() {
+    return {
+      tabsEeui:{ 
+        tabType: 'top',
+        tabBackgroundColor: '#ffffff',
+        indicatorColor: '#3075ee',
+        indicatorWidth: 160,
+        underlineColor: '#e9e9e9',
+        underlineHeight: 2,
+        textSelectColor: '#3075ee',
+        textUnselectColor: '#999999',
+        textBold:2 
+      },
+      asset: {},
+      assetAddress:'0x58Df7EDadC0795519fdC7284A67547670F08C5E0',
+      tabPages: [
+        {
+            title: '全部',
+            url: 'http://dotwe.org/raw/dist/b5fd96d8d790f0100bdfc20b93eedf09.bundle.wx',
+        },
+        {
+            title: '收款',
+            url: 'http://dotwe.org/raw/dist/ba938c9aaebe41e5f60b98f90bd0bf61.bundle.wx',
+            message: 9
+        },
+        {
+            title: '转账',
+            url: 'http://dotwe.org/raw/dist/fb6f016b0116969b6b503e1d3a35285f.bundle.wx',
+            message: 18
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.asset = app.config.params ? app.config.params : "error";
+  },
+  methods: {
+    receiptClick() {
+      eeui.openPage({
+        url: 'receiptDetails',
+        pageType: "app",
+        params: this.asset,
+      });
+    },
+    transferClick() {
+      eeui.openPage({
+        url: 'transferDetails',
+        pageType: "app",
+        params: this.asset,
+      });
+    },
+    copyText() {
+      eeui.copyText(this.assetAddress);
+      eeui.toast({
+          message: "复制成功",
+          gravity: "middle"
+        });
+    },
+    cropyText(str){
+      let t = ""
+      t = str.slice(0,10) + "******" + str.substring(32)
+      return t
+    }
+  }
+};
+</script>
+
+<style scoped>
+.assetsdetails {
+  width: 750px;
+  flex: 1;
+}
+.assetsdetails-navbar {
+  width: 750px;
+  height: 100px;
+}
+
+.assetsdetails-navbar-main {
+  flex-direction: row;
+  align-items: center;
+}
+
+.assetsdetails-navbar-title {
+  font-size: 32px;
+  color: #ffffff;
+}
+.assetsdetails-navbar-right {
+  width: 100px;
+  height: 100px;
+  color: #ffffff;
+}
+.assetsdetails-content {
+  margin-top: 64px;
+  width: 750px;
+  flex: 1;
+  align-items: center;
+  /* justify-content: center; */
+}
+.assetsdetails-content-logo {
+  width: 128px;
+  height: 128px;
+}
+.assetsdetails-content_asset {
+  align-items: center;
+  margin-top: 32px;
+}
+.assetsdetails-content_assetvalue {
+  font-size: 80px;
+  font-weight: 500;
+  color: #333333;
+}
+.assetsdetails-content_assetusd {
+  font-size: 40px;
+  font-weight: 500;
+  color: #666666;
+}
+.assetsdetails-content_line {
+  background-color: #f8f9fa;
+  height: 32px;
+  width: 750px;
+}
+
+.assetsdetails-content_button {
+  font-size: 24px;
+  margin-left: 38px;
+  margin-right: 38px;
+  margin-top: 64px;
+  margin-bottom: 20px;
+  width: 300px;
+  height: 80px;
+}
+.assetsdetails-content_listitem {
+  width: 750px;
+  flex-direction: row;
+  margin-bottom: 32px;
+}
+
+.assetsdetails-content_tabs {
+  flex: 1;
+  width: 750px;
+}
+</style>
